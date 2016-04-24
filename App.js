965cd9958bf7178996a07476147e269ce880237a
sminutoli <sminutoli.it@gmail.com>
2016-04-23 22:03:48 -0300

    Adds component using React.createClass and Refs

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Slider from './Slider';
 import Widget from './Widget';
 
 class App extends React.Component {
@@ -9,22 +11,37 @@ class App extends React.Component {
       another: 0
     }
     this.update = this.update.bind(this);
+    this.updateColor = this.updateColor.bind(this);
   }
 
   update(evt){
     this.setState({ stateVal: evt.target.value })
   }
 
+  updateColor(evt){
+    var { red, green, blue } = this.refs;
+    this.setState({
+      red: ReactDOM.findDOMNode(red).value,
+      green: ReactDOM.findDOMNode(green).value,
+      blue: ReactDOM.findDOMNode(blue).value
+    });
+  }
+
   render(){
-    let { val, val2 } = this.props;
-    let { stateVal:st } = this.state;
+    var { val, val2 } = this.props;
+    var { stateVal:st, red, green, blue } = this.state;
+    var css = {
+      color: 'rgb('+red+','+green+','+blue+')'
+    };
     return (
       <div>
-        <h1>Hola {val} {val2}</h1>
+        <h1 style={css}>Hola {val} {val2}</h1>
         <h2>{st}</h2>
         <Widget update={this.update} value={st} />
         <Widget update={this.update} value={st} />
-        <Widget update={this.update} value={st} />
+        <Slider ref="red" change={this.updateColor} />
+        <Slider ref="green" change={this.updateColor} />
+        <Slider ref="blue" change={this.updateColor} />
       </div>
       );
   }
---
+++ Slider.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Slider = React.createClass({
+  render(){
+    return <input type="range" min="0" max="255" onChange={this.props.change} />
+  }
+}); 
+
+export default Slider;
